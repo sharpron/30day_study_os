20860e25618f76a86a7fb56003bb466da09f37b1
ron <1358612584@qq.com>
2020-02-22 18:52:03 +0800

    finish makefile

--- day3/ipl.asm
+++ day3/ipl.asm
@@ -1,3 +1,6 @@
+; 读取柱面数量
+
+C_	EQU		10
 
 ; initial program loader
 
@@ -25,7 +28,7 @@
 	DD		0xffffffff
 	DB		"LIGHT-OS   "
 	DB		"FAT12   "
-	RESB    18
+	TIMES	18 DB 0x00
 
 ; 程序入口
 	
@@ -35,29 +38,83 @@ init:
 	MOV		SP, 0x7c00	
 	MOV		DS, AX
 	MOV		ES, AX
-	
-	MOV		SI, msg
 
-show_msg:
-	MOV		AL, [SI]
+; 读取软盘数据
+read_floppy:
+	MOV		AX, 0x0820
+	MOV		ES, AX
+	MOV		BX, 0
+	
+	MOV		CH, 0				; 柱面0
+	MOV		DH, 0				; 磁头0
+	MOV		CL, 2				; 扇区2
+
+read_loop:
+	MOV		SI, 0
+	
+read_retry:
+	
+	MOV		AH, 0x02			; 模式为读盘
+	MOV		AL,	1				; 连续读1个扇区
+	MOV		DL, 0				; A驱动器
+	INT		0x13				; 调用磁盘bios
+	JNC		read_next
+	
 	INC		SI
+	CMP		SI, 5
+	JA		error
+	MOV		AH, 0
+	MOV		DL, 0
+	INT		0x13
+	
+	JMP		read_retry
+
+read_next:
+	MOV  	AX, ES
+	ADD		AX, 32
+	MOV		ES, AX				; 将缓存区位置移动512个字节
+	
+	INC		CL					; 读下一个扇区
+	CMP		CL,	18				; 比较扇区号是否大于18
+	JBE		read_loop			
+	
+	MOV		CL, 1				; 重置扇区，磁头号进位
+	INC		DH					; 读下一个磁头
+	CMP		DH, 2				; 共有两个磁头
+	JB		read_loop
+	
+	MOV		DH, 0				; 重置磁头号，柱面进位
+	INC		CH					; 读下一个柱面
+	CMP		CH, C_				
+	JB		read_loop
+			
+    JMP		0xc200				; 跳转到系统执行	
+	
+	
+; 显示错误消息
+error:
+	MOV		SI, msg_error
+
+putloop:
+	MOV		AL, [SI]
+	ADD		SI, 1
 	CMP		AL, 0
 	JE	    fin	
 	MOV		AH, 0x0e
 	MOV		BX, 15
 	INT		0x10
-	JMP		show_msg
+	JMP		putloop
 
 ; 结束运行
 fin:
 	HLT
 	JMP		fin
 
-msg:
-	DB		0x0a, 0x0a
-	DB		"welcome play light os!"
-	DB		0x0a
-	DB		0
+msg_error:
+	DB		"read disk error"
+	DB		0x00
+	DB		0x00
+	DB 		0x00
 	
 	TIMES	0x1fe-($-$$) DB	0x00
 	DW 		0xAA55
